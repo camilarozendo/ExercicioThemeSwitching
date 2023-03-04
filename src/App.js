48fb243bb2f5c4787c5a5cb06a23661e1888f8d0
Camila Rozendo <camilarozendo@hotmail.com>
2023-03-03 23:14:19 -0300

    exercicio05

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import {
+  ThemeContainer,
+  ThemeButton,
+} from "./components/styles/ThemeSwitching.styled";
+
+import { ThemeProvider } from "styled-components";
+import {
+  light,
+  dark,
+  blue,
+  green,
+  brown,
+  pink,
+} from "./components/styles/Theme.styled";
+
+import { GlobalStyles } from "./components/styles/Global";
+import { Header } from "./components/styles/Header.styled";
+import { Footer } from "./components/styles/Footer.styled";
+import Quotes from "./components/Quotes";
+
 
 function App() {
+// theme state
+  const [selectedTheme, setSelectedTheme] = useState(light);
+// react hook to get the theme selected by the user that is saved in local storage
+useEffect(() => {
+  const currentTheme = JSON.parse(localStorage.getItem("current-theme"));
+  if (currentTheme) {
+    setSelectedTheme(currentTheme);
+  }
+}, []);
+
+  const invertedSelectedTheme = ({ colors }) => ({
+    colors: {
+      background: colors.footer,
+      footer: colors.background,
+    },
+  });
+
+
+// function to handle user theme selection on click and save it to local storage
+  const HandleThemeChange = (theme) => {
+    setSelectedTheme(theme);
+    localStorage.setItem("current-theme", JSON.stringify(theme));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeProvider theme={selectedTheme}>
+      <div className="App">
+    <GlobalStyles />
+      <Header>Game of Thrones Quotes</Header>
+
+      <ThemeContainer>
+        <span>Themes: </span>
+        <ThemeButton
+            className={`light ${selectedTheme === light ? "active" : ""}`}
+            onClick={() => HandleThemeChange(light)}></ThemeButton>
+          <ThemeButton
+            className={`dark ${selectedTheme === dark ? "active" : ""}`}
+            onClick={() => HandleThemeChange(dark)}></ThemeButton>
+          <ThemeButton
+            className={`blue ${selectedTheme === blue ? "active" : ""}`}
+            onClick={() => HandleThemeChange(blue)}></ThemeButton>
+          <ThemeButton
+            className={`green ${selectedTheme === green ? "active" : ""}`}
+            onClick={() => HandleThemeChange(green)}></ThemeButton>
+          <ThemeButton
+            className={`brown ${selectedTheme === brown ? "active" : ""}`}
+            onClick={() => HandleThemeChange(brown)}></ThemeButton>
+          <ThemeButton
+            className={`pink ${selectedTheme === pink ? "active" : ""}`}
+            onClick={() => HandleThemeChange(pink)}></ThemeButton>
+      </ThemeContainer>
+
+      <ThemeProvider theme={selectedTheme}>
+        <Quotes />
+      </ThemeProvider>
+      
+      <Footer>
+          <p>
+            Made with love by <a href="https://www.linkedin.com/in/camilarozendo/">Camila</a>
+          </p>
+      </Footer>
+      <ThemeProvider theme={invertedSelectedTheme}>
+  <Footer>
+    <p>
+     Inverted Footer
+    </p>
+  </Footer>
+</ThemeProvider>
     </div>
+    </ThemeProvider>
   );
 }
 
